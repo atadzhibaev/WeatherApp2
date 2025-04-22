a9e07159bf0663bae44369a6c27187147e234b3f
Aziz Tadzhibaev <atadzhibaev@northeast.edu>
2025-04-22 12:39:59 -0500

    added weather data object, still need to create weather service class

---
+++ src/WeatherData.java
@@ -0,0 +1,75 @@
+public class WeatherData {
+    private String location;
+    private double temperature;
+    private String weatherDescription;
+    private String state;
+    private String placeName;
+    private String country;
+    private double windDirection;
+    private double windSpeed;
+
+    public WeatherData(String location, double temperature, String weatherDescription, double windDirection, double windSpeed) {
+        this.location = location;
+        this.temperature = temperature;
+        this.weatherDescription = weatherDescription;
+        this.windDirection = windDirection;
+        this.windSpeed = windSpeed;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public double getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(double windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    //helper method to convert degrees (angles) to wind direction
+    private String getCompassDirection(double angle) {
+        String[] directions = {"North", "Northeast", "East", "Southeast", "South", "Southwest", "West", "NorthWest", "North"};
+        return directions[(int) Math.round(((angle % 360)/45))];
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "location='" + location + '\'' +
+                ", temperature=" + temperature +
+                ", weatherDescription='" + weatherDescription + '\'' +
+                ", windDirection=" + windDirection +
+                ", windSpeed=" + windSpeed +
+                '}';
+    }
+}
